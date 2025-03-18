9439453e6e6caa941c5e1acbe49becc7da744633
realmizzer <diamondAlma32@gmail.com>
2025-03-18 07:50:30 +0700

    Init & completed a first lesson «Variables»

---
+++ MyPlayground.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// mutable variable declaration
+var firstName = "Maxim"
+print(firstName)
+
+var stockprice = 100
+print(stockprice)
+
+stockprice = 50
+print(stockprice)
+
+// const
+let lastName = "Nikolaev"
