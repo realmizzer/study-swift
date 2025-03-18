1930e2b01942183181b2bd6fc83b42c2552e2c74
realmizzer <diamondAlma32@gmail.com>
2025-03-18 08:13:14 +0700

    Completed second lesson «Data Types»

---
+++ MyPlayground.playground/Pages/DataTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+var str: String = "String"
+
+var number: Int = 42
+
+var decimal32: Float = 3.14
+
+// Double is default type for decimal numbers.
+var decimal64: Double = 3.14159
+
+var bool: Bool = true
