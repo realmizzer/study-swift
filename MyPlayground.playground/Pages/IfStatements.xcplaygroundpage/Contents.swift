da47b7c7d60fc585ad5777b856c9318a60a92897
realmizzer <diamondAlma32@gmail.com>
2025-03-18 08:31:51 +0700

    actually, this commit completed the third lesson

--- MyPlayground.playground/Pages/IfStatements.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/IfStatements.xcplaygroundpage/Contents.swift
@@ -1,7 +1,19 @@
-//: [Previous](@previous)
+let x = 10
+let y = 5
+let z = 42
 
-import Foundation
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+if x < 10 {
+    print("x is less than 10")
+}
+else if x >= 5 && y >= 5 {
+    print("x and y equals or greater than 5")
+}
+else if x >= 5 || y >= 5 {
+    print("x or y equals or greater than 5")
+}
+else if (x >= 5 || y >= 5) && z == 42 {
+    print("x or y more or greater than 5 but 42 is an answer")
+}
+else {
+    print("x is greater than 10")
+}
