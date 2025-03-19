34bbefae58c4cada74fa9c1f025a8da5adb709d1
realmizzer <diamondAlma32@gmail.com>
2025-03-19 23:43:01 +0700

    Completed 10th lesson «Subclassing and Inheritance»

--- MyPlayground.playground/Pages/Classes.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -1,6 +1,10 @@
-class Employee {
+class Person {
     
-    var name = "Unkown employee"
+    var name = ""
+}
+
+class Employee: Person {
+    
     var salary = 0
     var currencyType = "Dollar"
     var role = "Employee"
@@ -12,9 +16,33 @@ class Employee {
     
 }
 
+class Manager: Employee {
+    
+    var teamSize = 0
+    
+    override func doWork() {
+//        super.doWork()
+        
+        print("\(name) is managing people")
+        salary += 2
+    }
+    
+    func fire() {
+        print("\(name) fired somebody")
+    }
+    
+}
+
 let emp1 = Employee()
 emp1.name = "Maxim"
 emp1.salary = 10000
 emp1.role = "Developer"
-
 emp1.doWork()
+
+let manager = Manager()
+manager.name = "Artem"
+manager.salary = 10000
+manager.role = "Manager"
+manager.teamSize = 30
+manager.doWork()
+manager.fire()
