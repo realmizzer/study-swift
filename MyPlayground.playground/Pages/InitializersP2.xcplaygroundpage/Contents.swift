7eeac9a02d777f087b714e1d0e3a51e4b7dac1c7
realmizzer <diamondAlma32@gmail.com>
2025-04-03 10:25:01 +0700

    Completed 15th lesson «Designated and Convenience Initializers»

--- MyPlayground.playground/Pages/InitializersP2.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/InitializersP2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
-//: [Previous](@previous)
+class Person {
+    
+    var name: String?
+    var netWorth: Int?
+    var gender: String!
+    
+    // Designated initializer, because it makes sure that all properties are initialized
+    init() {
+        name = "None"
+    }
+    
+    convenience init(_ gender: String, _ netWorth: Int) {
+        
+        self.init()
+        
+        self.gender = gender
+        self.netWorth = netWorth
+        
+    }
+    
+}
 
-import Foundation
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+let person1 = Person()
+let person2 = Person("Make", 10230382038)
